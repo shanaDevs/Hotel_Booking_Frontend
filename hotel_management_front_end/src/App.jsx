dabcea991890dfc889414ae4752ca33299fb8627
shanaDevs <94227127+Shyamalpushpakumara@users.noreply.github.com>
2024-11-01 23:18:22 +0530

    lgin and backend implementation done

--- hotel_management_front_end/src/App.jsx
+++ hotel_management_front_end/src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/clientpage/homepage";
 import { AdminPage } from "./pages/adminpage/admin";
+import LoginPaage from "./pages/login/login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/Admin/*" element={<AdminPage />} />
+      <Route path="/admin/*" element={<AdminPage />} />
+      <Route path="/login/*" element={<LoginPaage />} />
         <Route path="/" element={<Homepage />} />
       </Routes>
     </BrowserRouter>
