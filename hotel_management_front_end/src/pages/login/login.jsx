dcf433ac18eae90d5ecf9bf4a67f4930b623e732
shanaDevs <94227127+Shyamalpushpakumara@users.noreply.github.com>
2024-11-02 13:33:33 +0530

    login and backend implementation done

---
+++ hotel_management_front_end/src/pages/login/login.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react"
+import axios from "axios"
+import "./login.css"
+export default function LoginPage(){
+
+    const [email , setEmail] = useState("")
+    const [password , setPassword] = useState("")
+
+    function handleLogin (){
+        axios.post(import.meta.env.VITE_BACKEND_URL+"/api/users/login",{
+            email:email,
+            password:password,
+        }).then(
+            (res) =>{
+                localStorage.setItem("token",res.data.token)
+                if(res.data.user.type ==="admin"){
+                    window.location.href="/admin/"
+                }else{
+                window.location.href="/"
+                }
+            }
+        ).catch((err)=>{
+            console.log(err)
+            alert("Invalid credentials")
+        })
+            }
+
+    return(
+        <div className="w-full h-[100vh] pic-bg flex justify-center items-center">
+            <div className="w-[400px] h-[400px] backdrop-blur-md rounded-lg flex flex-col items-center justify-center">
+                <h1 className="text-white text-4xl p-[20px] text-center absolute top-[40px]">Login</h1>
+                <input type="text" placeholder="Enter your email address" className="w-[80%] bg-[#0000] border-[2px] text-white outline-none h-[50px] px-[5px] mb-[5px]" defaultValue={email} onChange={(e)=>{
+                    setEmail(e.target.value)
+                }}/>
+                <input type="password" placeholder="Enter your password" className="w-[80%] bg-[#0000] border-[2px] text-white outline-none h-[50px] px-[5px]" defaultValue={password} onChange={
+                    (p)=>{
+                    setPassword(p.target.value)
+                }}/>
+                <button className="w-80 absolute bottom-[40px] bg-red-500 text-white h-[50px]" onClick={handleLogin}>Login</button>
+            </div>
+        </div>
+    )
+}
